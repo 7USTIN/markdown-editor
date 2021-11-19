21d829c3c7b4ce4e2abb7f8595ad45e31a203896
7USTIN <justin.faak@icloud.com>
2021-11-19 18:36:25 +0100

    initial setup

---
+++ src/App.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+</script>
+
+<main>
+	<h1>Hello World</h1>
+</main>
+
+<style>
+	:global(:root) {
+		--white: #ffffff;
+		--white-dark: #f2f2f2;
+		--black: #24292e;
+		--gray: #babbbd;
+	}
+
+	:global(*) {
+		margin: 0;
+		padding: 0;
+		box-sizing: border-box;
+		outline: none;
+	}
+
+	:global(body, button, input) {
+		font-family: "Inter", sans-serif;
+		font-size: 14px;
+		background: var(--white);
+		color: var(--black);
+	}
+</style>
